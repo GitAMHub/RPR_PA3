25e35e0695fdce4e2cb54ce97bc2d8dfbdedb7d3
gitAMhub <buzono-github@yahoo.com>
2017-03-12 23:53:45 +0100

    Cambios

--- best.R
+++ best.R
@@ -2,6 +2,10 @@ best <- function(state, outcome) {
         
         ## Read outcome data
         outcome <- read.csv("outcome-of-care-measures.csv",colClasses = "character")
+        outcome[, 11] <- as.numeric(outcome[, 11])
+        outcome[, 11] <- as.numeric(outcome[, 11])
+        outcome[, 17] <- as.numeric(outcome[, 17])
+        outcome[, 23] <- as.numeric(outcome[, 23])
         
         
         ## Check that state and outcome are valid
